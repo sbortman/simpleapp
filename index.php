7dd01061601f1173eebb5f44efd4be1d9ac1fb41
Scott Bortman <scott.bortman@gmail.com>
2020-04-21 15:20:22 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 	</head>
 
 <body> 
-	<h1>PHP Test</h1>
-		<p><b>An Example of PHP in Action</b></p>
+	<h1>My PHP Test</h1>
+		<p><b>My Example of PHP in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information</h2> 
